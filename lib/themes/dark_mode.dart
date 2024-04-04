b73a353db083d7167fefd336c87c37f9c5aab32a
Mike Ofori <102876495+michaelofori@users.noreply.github.com>
2024-04-04 22:25:54 +0000

    j

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode =ThemeData(
+  colorScheme: ColorScheme.light(
+    background: const Color.fromARGB(255, 20, 20, 20),
+    primary: const Color.fromARGB(255, 105, 105, 105),
+    secondary: Color.fromARGB(255, 80, 80, 80),
+    tertiary: const Color.fromARGB(255, 44, 44, 44),
+    inversePrimary: const Color.fromARGB(255, 41, 10, 10),
+  ),
+);
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode =ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.red.shade300,
+    primary: Colors.red.shade500,
+    secondary: Colors.red.shade100,
+    tertiary: Colors.red,
+    inversePrimary: Colors.red.shade700,
+  ),
+);
